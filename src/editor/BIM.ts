0967af8cdbb2a47e179d2428f876bfc89fdb2782
legriamax <legriamax@proton.me>
2022-09-06 18:19:54 +0000

    Add more description

--- src/editor/BIM.ts
+++ src/editor/BIM.ts
@@ -1,3 +1,18 @@
 import Imgr from "./base/mgr";
 import ServiceContainer from "./base/server/ServiceContainer";
-import { E
+import { EventDispatcher } from "./framework/event/EventDispatcher";
+import { KeyBoardManager } from "./framework/event/KeyBoardManager";
+import Service from "./service";
+
+export default class BIM {
+
+    static mode: number;
+
+    static container: any;
+
+    static uicontainer:any;
+    
+    /** 管理 */
+    static readonly MGR: Imgr = new Imgr();
+    /** 全局服务容器 */
+    static readonly SC: ServiceCo

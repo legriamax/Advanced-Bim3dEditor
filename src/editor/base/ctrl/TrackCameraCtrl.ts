0a72eaaea4f3735190bde767330930bddb6bc681
legriamax <legriamax@proton.me>
2022-10-15 08:47:06 +0000

    ASoC: omap-mcbsp: Fix 6pin mux configuration

--- src/editor/base/ctrl/TrackCameraCtrl.ts
+++ src/editor/base/ctrl/TrackCameraCtrl.ts
@@ -467,4 +467,12 @@ export default class TrackCameraCtrl implements IDispose {
 
             // let x = this.camera.position.y >= 0 ? -this._eye.x : this._eye.x;
             // let z = this.camera.position.y >= 0 ? -this._eye.z : this._eye.z;
-            le
+            let front = new Vector3(-this._eye.x, 0, -this._eye.z);
+            pan.add(objectUp.copy(front).setLength(mouseChange.z));
+        }
+
+        // 移动相机位置和聚焦点位置，保持eye向量不变
+        // 这里保证无论怎么旋转场景，当鼠标做平移操作时，场景都是平行于屏幕上下移动的，而不是相对于他们自己的模型坐标系移动
+        // this.camera.up.y < 0 ? this.camera.position.sub(pan):
+        this.camera.position.add(pan);
+        //  如果焦点没有移动我们相当于一直在盯着一个

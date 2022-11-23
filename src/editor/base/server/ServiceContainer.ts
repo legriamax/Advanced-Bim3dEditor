5e38427f0032a7ee5e545e73bd4ed01e4d4b36d8
legriamax <legriamax@proton.me>
2022-11-23 05:51:38 +0000

    Added some fixes

--- src/editor/base/server/ServiceContainer.ts
+++ src/editor/base/server/ServiceContainer.ts
@@ -1,3 +1,26 @@
 import { service } from "@/libs/const/enum";
 import ServiceBase from "./ServiceBase";
-import ServiceMa
+import ServiceManager from "./ServiceManager";
+
+/**
+ * @description 服务容器
+ * @author songmy
+ */
+export default class ServiceContainer {
+
+    private _scheme: any;
+
+    private _cad: any;
+
+    private _scene: any;
+
+    private _mesh: any;
+
+    /**
+     * 是否存在服务<br>
+     * @param type
+     * @return
+     *
+     */
+    static hasService(type: string): boolean {
+        return ServiceManager.ins.getS

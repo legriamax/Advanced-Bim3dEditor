709b498cd09f8f27ffaa8ba616c19344fa1a668b
legriamax <legriamax@proton.me>
2022-12-10 21:41:00 +0000

    release v0.5.1

---
+++ src/libs/const/enum.ts
@@ -0,0 +1,8 @@
+export const enum service {
+    /** cad服务 */
+    CAD_SERVICE = 'service.cad',
+    /** 方案版本服务 */
+    SCHEME_SERVICE = 'service.scheme',
+    /** 场景服务 */
+    SCENE_SERVICE = 'service.scene',
+    /** 建模服务

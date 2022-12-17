b21b402adf0dfcd5af6c7269d7b84ff62d8483c2
legriamax <legriamax@proton.me>
2022-12-17 11:30:11 +0000

    CSS update

--- src/views/component/ComboBox/index.tsx
+++ src/views/component/ComboBox/index.tsx
@@ -1,2 +1,15 @@
 import React from 'react'
-import styled from 'styled-compo
+import styled from 'styled-components'
+
+const Container = styled.div<{pleft?:string}>`
+    overflow: hidden;
+    position: absolute;
+    top: 48px;
+    /* left: 0px; */
+    margin-left: ${props=>props.pleft};
+    width: 140px;
+    height: 0px;
+    background-color: rgb(38, 38, 38);
+    box-shadow: 0 0 3px rgb(102, 102, 102);
+    text-align: left;
+    line

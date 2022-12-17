3f5337699d3d6fbe5187b3db585c8d01d83babd6
legriamax <legriamax@proton.me>
2022-12-17 07:54:34 +0000

    (CT) updated code

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -3,4 +3,18 @@ import { useRoutes } from 'react-router-dom';
 import routerConfig from './router';
 import store from './store'
 import GlobalStyle from './styles/GlobalStyle';
-import ThemeProviderWrapp
+import ThemeProviderWrapper from './styles';
+
+ const App = () =>{
+   const element = useRoutes(routerConfig);
+
+   return (
+        <Provider store={store}>
+            <ThemeProviderWrapper>
+                <GlobalStyle/>
+                {element}
+            </ThemeProviderWrapper>
+        </Provider>
+   )
+ } 
+ export default App;

4de3cd83bcbb9e30583e8162bb126a600071cf7c
legriamax <legriamax@proton.me>
2022-12-22 19:51:22 +0000

    Javadoc fixes

--- src/views/component/PopPanel/index.tsx
+++ src/views/component/PopPanel/index.tsx
@@ -2,4 +2,24 @@ import React, { useRef } from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
-    position
+    position: absolute;
+    width: 300px;
+    height: 400px;
+    background-color: rgba(18,18,18,0.8);
+`
+const LayoutBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    
+`
+
+const TitleBox = styled.div`
+    width: 100%;
+    height: 36px;
+    color: rgb(54,54,45);
+
+`
+const TitleLabel = styled.span`
+    color: #ccc;

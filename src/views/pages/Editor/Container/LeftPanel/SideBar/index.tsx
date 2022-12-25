dd70c114d8afe1e6cd6ca33872d8e58dc8e613dc
legriamax <legriamax@proton.me>
2022-12-25 22:59:01 +0000

    Adjust code style

--- src/views/pages/Editor/Container/LeftPanel/SideBar/index.tsx
+++ src/views/pages/Editor/Container/LeftPanel/SideBar/index.tsx
@@ -1,3 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Building2 } from '@styled
+import { Building2 } from '@styled-icons/remix-line/Building2'
+import { ImageEdit } from '@styled-icons/fluentui-system-regular/ImageEdit'
+import { Molecule } from '@styled-icons/fluentui-system-regular/Molecule'
+import { TextParagraph } from '@styled-icons/fluentui-system-regular/TextParagraph'
+
+const SideBarBox = styled.div`
+    width: 48px;
+    height: 100%;
+    background-co

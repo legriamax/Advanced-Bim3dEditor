f864c0d4d5580f1f2de087260ab97d754f202a6d
legriamax <legriamax@proton.me>
2022-12-26 07:03:34 +0000

    target: Fix incorrect usage of nested IRQ spinlocks in ABORT_TASK path

--- src/views/pages/Editor/Container/LeftPanel/SideBar/index.tsx
+++ src/views/pages/Editor/Container/LeftPanel/SideBar/index.tsx
@@ -8,4 +8,24 @@ import { TextParagraph } from '@styled-icons/fluentui-system-regular/TextParagra
 const SideBarBox = styled.div`
     width: 48px;
     height: 100%;
-    background-co
+    background-color: rgb(18,18,18);
+`
+const LeftMenuItem = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 60px;
+    color: white;
+    font-size: 12px;
+    &:hover{
+        background-color:rgb(242, 112, 19);
+    }
+`
+
+const Building2Icon = styled(Building2)`
+    color: white;
+
+`
+const ImageEditIcon = st

fc4535493d89ea208388357a8bf3239fb896dd7c
legriamax <legriamax@proton.me>
2022-12-31 10:13:46 +0000

    schedule update

--- src/views/pages/Editor/Container/RightPanel/PropsPanel/index.tsx
+++ src/views/pages/Editor/Container/RightPanel/PropsPanel/index.tsx
@@ -1,2 +1,21 @@
 import React from 'react'
-imp
+import styled from 'styled-components'
+import TitleItem from './TitleItem'
+
+const PropsPanelBox = styled.div`
+    position: relative;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    margin-top: 2px;
+    color: white;
+    text-align: left;
+    line-height: 40px;
+    background-color: rgb(38,38,38);
+    border-radius: 5px;
+`
+
+const PropsBox = styled.div`
+    display: flex;
+    flex-dire

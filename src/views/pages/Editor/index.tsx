9ec7a835c81e5c0c442c0383287b4ff3f0c4ef24
legriamax <legriamax@proton.me>
2023-01-04 10:06:35 +0000

    added minified version

--- src/views/pages/Editor/index.tsx
+++ src/views/pages/Editor/index.tsx
@@ -1,3 +1,21 @@
 import BIM from '@/editor/BIM';
 import React from 'react'
-import style
+import styled from 'styled-components';
+import Container from './Container';
+
+const EditorDiv = styled.div`
+    position: absolute;
+    left: 0;
+    top: 0;
+    margin: 0;
+    padding: 0;
+    /* width: 100vh; */
+    /* height: 100vh; */
+    &::-webkit-scrollbar{
+        display: none;
+    }
+`
+
+class Editor extends React.Component {
+
+    container = React.createRef<HTMLDivEl

a16dfaba021a646c52a374e7c019797d87cb67e5
legriamax <legriamax@proton.me>
2023-01-08 20:14:29 +0000

    added missing images

--- src/views/pages/Home/Header/Logo/index.tsx
+++ src/views/pages/Home/Header/Logo/index.tsx
@@ -9,4 +9,23 @@ interface LogoClor {
 const LogoBox = styled.div<LogoClor>`
     display: flex;
     align-items: center;
-    height: 
+    height: 100%;
+    width: 100%;
+    color: ${props=>props.primary?'black':'white'};
+    font-size: 1.2rem;
+    line-height: 1.57;
+    font-family: "Public Sans", sans-serif;
+    font-weight: bold;
+    text-align: left;
+    margin-left: 40px;
+    
+`
+
+const BlueSketch = styled(Sketch)`
+  color: rgb(242, 112, 19);
+  margin: 0 5px;
+`
+
+const Logo = (props:any) => {
+  return (
+ 

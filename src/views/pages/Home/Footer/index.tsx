b570e2d33c40798c35ff8705e220353d86541ac2
legriamax <legriamax@proton.me>
2023-01-08 10:28:37 +0000

    chore(package): update pre-commit to version 1.2.0

---
+++ src/views/pages/Home/Footer/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const FooterBox = styled.div`
+
+    position: relative;
+    background-color: rgb(38, 38, 38);
+    overflow: hidden;
+    min-height: 50vh;
+    z-index: 1;
+
+`
+
+const Footer: React.FC = ()=> {
+ 

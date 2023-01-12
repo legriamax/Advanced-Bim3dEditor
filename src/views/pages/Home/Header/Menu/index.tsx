dc8d11481b59f8997f8c327e1d940c7a1acb2a47
legriamax <legriamax@proton.me>
2023-01-12 14:55:09 +0000

    Removed commented code.

--- src/views/pages/Home/Header/Menu/index.tsx
+++ src/views/pages/Home/Header/Menu/index.tsx
@@ -3,4 +3,21 @@ import { toggleTheme } from '@/views/store/user';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import styled from 
+import styled from 'styled-components';
+
+const MenuBox = styled.div`
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+`;
+
+const MenuButton = styled.button`
+    border: 0;
+    background: none;
+    text-transform: uppercase;
+    color: white;
+    /* font-weight: bold; */
+    position: relative;
+    outline

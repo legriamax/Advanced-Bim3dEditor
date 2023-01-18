0ced94817cd9d4413f0afc523ff0bdc23d1495f7
legriamax <legriamax@proton.me>
2023-01-18 11:59:23 +0000

    atualizado

--- src/views/pages/Home/MainBox/ButtonGroup/index.tsx
+++ src/views/pages/Home/MainBox/ButtonGroup/index.tsx
@@ -6,4 +6,21 @@ import styled from 'styled-components'
 const ButtonBox = styled.div`
     display: flex;
     flex-direction: row;
-    j
+    justify-content: center;
+    padding: 10px 0 20px 0;
+`
+
+const BorderButton = styled.button`
+    width: 140px;
+    height: 32px;
+    background: transparent;
+    color: rgb(242, 112, 19);
+    background: none;
+    border: 1px solid rgb(242, 112, 19);
+    border-radius: 5px 5px;
+    font-size: 16px;
+    margin: 0 20px;
+
+    &:hover{
+        color: rgb(179, 78, 6);
+      

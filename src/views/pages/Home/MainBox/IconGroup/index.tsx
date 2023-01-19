dd98d96a5a500891ad276687b58c364f113c3da0
legriamax <legriamax@proton.me>
2023-01-19 14:56:34 +0000

    Version 1.1r21

--- src/views/pages/Home/MainBox/IconGroup/index.tsx
+++ src/views/pages/Home/MainBox/IconGroup/index.tsx
@@ -1,2 +1,8 @@
 import React from 'react'
-import { Github } from '@styled-icons/boxicons-lo
+import { Github } from '@styled-icons/boxicons-logos/Github'
+import { ReactLogo } from '@styled-icons/fa-brands/ReactLogo'
+import { Threedotjs } from '@styled-icons/simple-icons/Threedotjs'
+import { Typescript } from '@styled-icons/simple-icons/Typescript'
+import { Javascript } from '@styled-icons/fluentui-system-filled/Javascript'
+import {Gitee} from '@styled-icons/simple-icons/Gitee'
+import styled from 'styled-compon

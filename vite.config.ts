a2d095effe66a6f18617063bd029091f0dfba006
legriamax <legriamax@proton.me>
2023-02-26 09:28:12 +0000

    Release v3.2.0

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reac
+  plugins: [react()],
+  base:'./',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+})
